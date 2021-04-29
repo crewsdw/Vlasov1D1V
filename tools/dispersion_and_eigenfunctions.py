a83d2f3379c29180c522cb5d01419ac17627e8e5
Daniel Crews <dcrews@uw.edu>
2021-04-28 18:52:09 -0700

    more fixes to eigenfunction

--- tools/dispersion_and_eigenfunctions.py
+++ tools/dispersion_and_eigenfunctions.py
@@ -25,62 +25,69 @@ grid = pv.StructuredGrid(k3, u3, v3)
 
 # plasma dispersion functions / parameters
 # gaussian speeds
-vg0 = 0.0
+# vg0 = 0.0
 vg1 = 4.0
 vg2 = -4.0
-# hilbert transforms
+# Plasma dispersion function definition
 sqrt_pi = np.pi ** 0.5
+
+
 def pd_func(zeta):
     return 1j * sqrt_pi * np.exp(-zeta ** 2.0) * (1.0 + sp.erf(1j * zeta))
 
-# Z = (np.pi ** 0.5) * sp.wofz(z) / 1j
-# fh = 1.0 + z * Z
 
-vb0 = (z - vg0) / vt
+# Three possible beams (however many you want)
+# vb0 = (z - vg0) / vt
 vb1 = (z - vg1) / vt
 vb2 = (z - vg2) / vt
 
-### Don't trust fadeeva function
-Z0 = pd_func(vb0)
+# Build plasma dispersion functions
+# Z0 = pd_func(vb0)
 Z1 = pd_func(vb1)
 Z2 = pd_func(vb2)
 # fh = ( (1.0 + vb0 * Z0) + (1.0 + vb1 * Z1) + (1.0 + vb2 * Z2) ) / 3.0
-fh = 0.5 * ( (1.0 + vb1 * Z1) + (1.0 + vb2 * Z2) )
+fh = 0.5 * ((1.0 + vb1 * Z1) + (1.0 + vb2 * Z2))
 
 # dispersion function
 kx = 0.1
 D = 1.0 + np.tensordot(np.divide(1.0, k ** 2.0), fh, axes=0)
 Dk = 1.0 + fh / (kx ** 2.0)
 
-# distribution
-fp0 = - (v - vg0) / (0.5 * vt ** 2.0) * np.exp(-(v - vg0)**2.0 / vt**2.0) / (vt * sqrt_pi)
-fp1 = - (v - vg1) / (0.5 * vt ** 2.0) * np.exp(-(v - vg1)**2.0 / vt**2.0) / (vt * sqrt_pi)
-fp2 = - (v - vg2) / (0.5 * vt ** 2.0) * np.exp(-(v - vg2)**2.0 / vt**2.0) / (vt * sqrt_pi)
+# distribution gradient
+# fp0 = - (v - vg0) / (0.5 * vt ** 2.0) * np.exp(-(v - vg0) ** 2.0 / vt ** 2.0) / (vt * sqrt_pi)
+fp1 = - (v - vg1) / (0.5 * vt ** 2.0) * np.exp(-(v - vg1) ** 2.0 / vt ** 2.0) / (vt * sqrt_pi)
+fp2 = - (v - vg2) / (0.5 * vt ** 2.0) * np.exp(-(v - vg2) ** 2.0 / vt ** 2.0) / (vt * sqrt_pi)
 # fp = (fp0 + fp1 + fp2) / 3.0
 fp = 0.5 * (fp1 + fp2)
 
+# Domain length
 L = 2.0 * np.pi / kx
 x = np.linspace(0, L, num=100)
 
-z0 = 0 + 2.77j # 3.7305 - 1.866j#(5.2889 - 0.0003139j)))# (2.0 + 0.7741j)))# 1.499j)) # + fp / (v + 1.5j)
+# Eigenvalue solution (just eyeballed)
+z0 = 0 + 2.77j  # 3.7305 - 1.866j#(5.2889 - 0.0003139j)))# (2.0 + 0.7741j)))# 1.499j)) # + fp / (v + 1.5j)
+# Build eigenfunction
 v_part = np.divide(fp, (v - z0))
+eig = -1j * np.tensordot(np.exp(1j * kx * x), v_part, axes=0)  # * 0.01 * np.exp(1.768 * kx * 25)
 
-psi = -1j * np.tensordot(np.exp(1j * kx * x), v_part, axes=0) # * 0.01 * np.exp(1.768 * kx * 25)
+# Visualize
 x2 = np.tensordot(x, np.ones_like(v), axes=0)
 v2 = np.tensordot(np.ones_like(x), v, axes=0)
 
-f1 = np.exp(-(v-vg1) ** 2.0 / vt ** 2.0) / (vt * sqrt_pi)
-f2 = np.exp(-(v-vg2) ** 2.0 / vt ** 2.0) / (vt * sqrt_pi)
+# Build distribution
+f1 = np.exp(-(v - vg1) ** 2.0 / vt ** 2.0) / (vt * sqrt_pi)
+f2 = np.exp(-(v - vg2) ** 2.0 / vt ** 2.0) / (vt * sqrt_pi)
 f = 0.5 * np.tensordot(np.ones_like(x), (f1 + f2), axes=0)
 fpx = 0.5 * np.tensordot(np.ones_like(x), fp, axes=0)
 
-rpsi = np.real(psi) # + f
-cb = np.linspace(np.amin(rpsi), np.amax(rpsi), num=100)
+real_eig = np.real(eig)  # + f
+# color-bars
+cb = np.linspace(np.amin(real_eig), np.amax(real_eig), num=100)
 cbf = np.linspace(np.amin(f), np.amax(f), num=100)
 cbp = np.linspace(np.amin(fp), np.amax(fp), num=100)
 
 plt.figure()
-plt.contourf(x2, v2, rpsi, cb)
+plt.contourf(x2, v2, real_eig, cb)
 plt.xlabel(r'Position $x/\lambda_D$')
 plt.ylabel(r'Velocity $v/v_t$')
 plt.title(r'Difference from equilibrium $\Delta f = f(x,v) - f_0(v)$')
@@ -88,7 +95,7 @@ plt.colorbar()
 # plt.grid(True)
 plt.tight_layout()
 
-idx_z = np.where(np.absolute(v - 2.0) <= 1.0e-3) # np.amin(np.absolute(v)))
+idx_z = np.where(np.absolute(v - 2.0) <= 1.0e-3)  # np.amin(np.absolute(v)))
 idx_k = np.where(np.absolute(k - 0.2) <= 1.0e-2)
 # print(idx_z)
 # print(idx_k)
@@ -107,6 +114,7 @@ plt.ylabel('Imaginary phase velocity')
 
 plt.show()
 
+# Make 3D plot
 grid["vol"] = np.real(D).flatten(order='F')
 contours0 = grid.contour([0])
 # Imag zero contour
@@ -118,7 +126,7 @@ p.add_mesh(grid.outline(), color='k')
 p.add_mesh(contours0, color='r', label='Real zero')
 p.add_mesh(contours1, color='g', label='Imaginary zero')
 p.add_legend()
-p.show_grid(xlabel=r'Wavenumber (norm. to Debye length) x ' + str(scale), 
-            ylabel='Real phase velocity (norm. to thermal vel.)', 
+p.show_grid(xlabel=r'Wavenumber (norm. to Debye length) x ' + str(scale),
+            ylabel='Real phase velocity (norm. to thermal vel.)',
             zlabel='Imaginary phase velocity (norm. to thermal vel.)')
 p.show()
