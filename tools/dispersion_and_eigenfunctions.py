12199111b7b06826a69ff716872410116c60b2e8
Daniel Crews <dcrews@uw.edu>
2021-04-28 18:42:32 -0700

    Updated eigenfunction solver

--- tools/dispersion_and_eigenfunctions.py
+++ tools/dispersion_and_eigenfunctions.py
@@ -5,7 +5,7 @@ import scipy.special as sp
 import pyvista as pv
 
 # grids
-v = np.linspace(-8, 8, num=200)
+v = np.linspace(-8, 8, num=300)
 vx = np.tensordot(v, np.ones_like(v), axes=0)
 vy = np.tensordot(np.ones_like(v), v, axes=0)
 # complex phase velocity
@@ -17,7 +17,7 @@ vt = 2.0 ** 0.5
 om_p = 1.0
 
 # 3-grids
-scale = 1.0
+scale = 3.0
 k3 = np.tensordot(scale * k, np.ones_like(vx), axes=0)
 u3 = np.tensordot(np.ones_like(k), vx, axes=0)
 v3 = np.tensordot(np.ones_like(k), vy, axes=0)
@@ -26,8 +26,8 @@ grid = pv.StructuredGrid(k3, u3, v3)
 # plasma dispersion functions / parameters
 # gaussian speeds
 vg0 = 0.0
-vg1 = 6.0
-vg2 = -6.0
+vg1 = 4.0
+vg2 = -4.0
 # hilbert transforms
 sqrt_pi = np.pi ** 0.5
 def pd_func(zeta):
@@ -44,7 +44,7 @@ vb2 = (z - vg2) / vt
 Z0 = pd_func(vb0)
 Z1 = pd_func(vb1)
 Z2 = pd_func(vb2)
-# fh = ( (1.0 + vb0 * Z0) ) # + (1.0 + vb1 * Z1) + (1.0 + vb2 * Z2) ) / 3.0
+# fh = ( (1.0 + vb0 * Z0) + (1.0 + vb1 * Z1) + (1.0 + vb2 * Z2) ) / 3.0
 fh = 0.5 * ( (1.0 + vb1 * Z1) + (1.0 + vb2 * Z2) )
 
 # dispersion function
@@ -62,7 +62,8 @@ fp = 0.5 * (fp1 + fp2)
 L = 2.0 * np.pi / kx
 x = np.linspace(0, L, num=100)
 
-v_part = np.divide(fp, (v - (3.7305 - 1.866j))) # (5.2889 - 0.0003139j)))# (2.0 + 0.7741j)))# 1.499j)) # + fp / (v + 1.5j)
+z0 = 0 + 2.77j # 3.7305 - 1.866j#(5.2889 - 0.0003139j)))# (2.0 + 0.7741j)))# 1.499j)) # + fp / (v + 1.5j)
+v_part = np.divide(fp, (v - z0))
 
 psi = -1j * np.tensordot(np.exp(1j * kx * x), v_part, axes=0) # * 0.01 * np.exp(1.768 * kx * 25)
 x2 = np.tensordot(x, np.ones_like(v), axes=0)
@@ -80,9 +81,11 @@ cbp = np.linspace(np.amin(fp), np.amax(fp), num=100)
 
 plt.figure()
 plt.contourf(x2, v2, rpsi, cb)
-plt.xlabel('Position x')
-plt.ylabel('Velocity v')
+plt.xlabel(r'Position $x/\lambda_D$')
+plt.ylabel(r'Velocity $v/v_t$')
+plt.title(r'Difference from equilibrium $\Delta f = f(x,v) - f_0(v)$')
 plt.colorbar()
+# plt.grid(True)
 plt.tight_layout()
 
 idx_z = np.where(np.absolute(v - 2.0) <= 1.0e-3) # np.amin(np.absolute(v)))
@@ -98,6 +101,8 @@ plt.figure()
 # plt.contour(k3[:, 66, :], v3[:, 66, :], np.imag(D[:, 66, :]), 0, colors='b')
 plt.contour(u3[60, :, :], v3[60, :, :], np.real(Dk[:, :]), 0, colors='r')
 plt.contour(u3[60, :, :], v3[60, :, :], np.imag(Dk[:, :]), 0, colors='b')
+plt.xlabel('Real phase velocity')
+plt.ylabel('Imaginary phase velocity')
 # print(k3[60, 0, 0])
 
 plt.show()
